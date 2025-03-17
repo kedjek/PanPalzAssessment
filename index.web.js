5a81b11c5d4564c148d131bf5fbc89c72f285479
kedjek <kedjek177@gmail.com>
2025-03-17 19:30:27 -0400

    fixed initial load error

--- index.web.js
+++ index.web.js
@@ -1,24 +1,13 @@
 import { AppRegistry } from 'react-native';
-// import App from './Auth';
 import Home from './Home';
-import TermsAndConditions from './src/screens/TermsAndConditions';
-// AppRegistry.registerComponent('panPalz', () => App);
-
 import { UserProvider } from './src/data/UserContext';
 
 // Create a wrapper component that provides the required props
 const TermsAndConditionsWrapper = () => {
-  const mockNavigation = {
-    navigate: (screen) => console.log(`Navigate to: ${screen}`),
-    goBack: () => console.log('Go back')
-  };
 
   return (
     <UserProvider>
-      <TermsAndConditions 
-        navigation={mockNavigation}
-        route={{}}
-      />
+      <Home/>
     </UserProvider>
   );
 };
